d57c81c2530eb390d5b780cc81914b10de354890
Kurtyyy597 <kurtmarquez238@gmail.com>
2026-02-14 20:13:13 +0800

    Fix Production CORS for Post

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "https://car-inventory-frontend-v1ma58r2o-kurtyyy597s-projects.vercel.app",
+        "http://localhost:5173",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
